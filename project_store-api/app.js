264f356b5b5e57bd4300cd814416e435175e7f83
The Anh Phung <theanhwork1405@gmail.com>
2024-04-18 22:55:15 +0700

    Connect Database Successful

--- project_store-api/app.js
+++ project_store-api/app.js
@@ -1,3 +1,27 @@
 const express = require("express");
+const errHandlerMiddleWare = require("./middleware/error-handler");
+const notFoundMiddleWare = require("./middleware/not-found");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+
+//routes
+
+app.use(notFoundMiddleWare);
+app.use(errHandlerMiddleWare);
+
+const port = process.env.PORT || 3000;
+const url = process.env.MONGO_URI;
+
+const start = async () => {
+  try {
+    await connectDB(url);
+    app.listen(port, console.log(`Sever is listening on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
